a5bba2bbf82df15163e00b9db576a793b7ffdcf5
leelearner <35988602+leelearner@users.noreply.github.com>
2020-11-27 10:28:39 +0800

    Add files via upload

---
+++ testOfflineTraffic.py
@@ -0,0 +1,2 @@
+from trafficutils import label_dns_in_kafka_traffic
+label_dns_in_kafka_traffic("./dns_data.cap")
---
+++ testOnlineTraffic.py
@@ -0,0 +1,3 @@
+from trafficutils import  live_dns_detect_in_kafka_traffic
+
+live_dns_detect_in_kafka_traffic(interface="ens33", isBlockTraffic=True, blockTime=160)
